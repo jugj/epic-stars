7c9c2a0df9fafb8505b994f32b10aa2d068f004e
Samir <jugj@users.noreply.github.com>
2025-08-16 15:23:24 +0200

    Änderungen von Samir am 2025-08-16 15:23:24

---
+++ Assets/übung.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class übung : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      int lives = 0;
+    if(lives == 0){
+      Debug.Log("Game Over");
+    }
+    for(int i = 1; i <= 1; i++){
+    Debug.Log(i);
+}
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
